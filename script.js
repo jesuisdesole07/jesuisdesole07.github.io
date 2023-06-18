44f6469aa204d977f68babbb433c958967735ae7
jesuisdesole07 <howdoimissyou@gmail.com>
2023-06-18 22:58:49 +0800

    test

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
-window.onload = function() {
-    fetch('output.html')
-        .then(response => response.text())
-        .then(ascii => {
-            const asciiContainer = document.getElementById('asciiContainer');
-            asciiContainer.textContent = ascii;
-    	    processUserInput(); // Process user input initially
-        })
-        .catch(error => {
-            console.log('Error loading ASCII art:', error);
-        });
-
-};
-
 function processUserInput() {
-    const userInput = document.getElementById('userInput').value.trim();
-    const asciiContainer = document.getElementById('asciiContainer');
+	const userInput = document.getElementById('userInput').value.trim();
+	const asciiContainer = document.getElementById('asciiContainer');
 
-    if (userInput === '07') {
-        // Load the ASCII art again from ascii.html
-        fetch('output.html')
-            .then(response => response.text())
-            .then(ascii => {
-                asciiContainer.textContent = ascii;
-            })
-            .catch(error => {
-                console.log('Error loading ASCII art:', error);
-            });
-    } else {
-        // Replace spaces and newlines with empty string
-        let asciiText = asciiContainer.textContent.replace(/[\s\n]/g, '');
+	if (userInput === '07') {
+		// Load the ASCII art again from ascii.html
+		fetch('output.html')
+			.then(response => response.text())
+			.then(ascii => {
+				asciiContainer.textContent = ascii;
+			})
+			.catch(error => {
+				console.log('Error loading ASCII art:', error);
+			});
+	} else {
+		// Load the ASCII art again from ascii.html
+		fetch('output.html')
+			.then(response => response.text())
+			.then(ascii => {
+				asciiContainer.textContent = ascii;
 
-        // Add a newline before each "-"
-        asciiText = asciiText.replace(/-/g, '\n-');
+				// Replace spaces and newlines with empty string
+				ascii = ascii.textContent.replace(/[\s\n]/g, '');
 
-        // Update the ASCII art container with modified text
-        asciiContainer.textContent = asciiText;
-    }
+				// Add a newline before each "-"
+				ascii = ascii.replace(/-/g, '\n-');
+
+				// Update the ASCII art container with modified text
+				asciiContainer.textContent = ascii;
+
+			})
+			.catch(error => {
+				console.log('Error loading ASCII art:', error);
+			});
+	}
 }
 
